30dc120015da701b62da6647af461404d73e4963
Hussam-22 <hussam@hotmail.co.uk>
2025-07-23 14:41:54 +0400

    add language controller

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,8 @@ export default defineConfig({
   server: {
     host: true,
   },
+  i18n: {
+    locales: ["ar", "en"],
+    defaultLocale: "en",
+  },
 });
--- src/data/menu-items.js
+++ src/data/menu-items.js
@@ -1,6 +1,14 @@
-export const MENU_ITEMS = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Services", href: "/services" },
-  { name: "Contact", href: "/contact-us" },
-];
+export const MENU_ITEMS = {
+  en: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Services", href: "/services" },
+    { name: "Contact", href: "/contact-us" },
+  ],
+  ar: [
+    { name: "الرئيسية", href: "/ar" },
+    { name: "معلومات عنا", href: "/ar/about" },
+    { name: "الخدمات", href: "/ar/services" },
+    { name: "اتصل بنا", href: "/ar/contact-us" },
+  ],
+};
